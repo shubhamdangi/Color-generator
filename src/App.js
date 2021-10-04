fb3ec6f899995387c8858ddcee998b3cb7ed28ef
shubhamdangi <shubham.d3v@gmail.com>
2021-10-04 16:26:41 +0530

    updated

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+import Values from "values.js";
+import SingleColor from "./SingleColor";
+
+function App() {
+  const [color, setColor] = useState("");
+  const [error, setError] = useState(false);
+  const [list, setList] = useState(new Values("#FF7777").all(10));
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    try {
+      let colors = new Values(color).all(10);
+      setList(colors);
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <section className="container">
+        <h3>Color Generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="#FF7777"
+            value={color}
+            onChange={(e) => setColor(e.target.value)}
+            className={`${error ? "error" : null}`}
+          />
+          <button className="btn" type="submit">
+            Get Colors
+          </button>
+        </form>
+      </section>
+      <section className="colors">
+        {list.map((color, index) => {
+          return (
+            <SingleColor
+              key={index}
+              {...color}
+              index={index}
+              hexColor={color.hex}
+            />
+          );
+        })}
+      </section>
+    </>
+  );
+}
+
+export default App;
